ac7d2b4933bedad4a7b365dab83395c8204e2a7c
Yohann Lepage <yohannlepage@gmail.com>
2009-02-10 15:33:28 +0100

    Arrêt du démarrage automatique des services

---
+++ config/chroot_local-hooks/99-stop.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+update-rc.d  -f mysql remove
+update-rc.d  mysql stop  0 1 2 3 4 5 6 .
+update-rc.d  -f apache2 remove
+update-rc.d  apache2 stop  0 1 2 3 4 5 6 .
+update-rc.d  -f bind9 remove
+update-rc.d  bind9 stop  0 1 2 3 4 5 6 .
+update-rc.d  -f puppet remove
+update-rc.d  puppet stop  0 1 2 3 4 5 6 .
+update-rc.d  -f puppetmaster remove
+update-rc.d  puppetmaster stop  0 1 2 3 4 5 6 .
