5632d50dbba9aec5b0516e3b5710fd2a089a82ac
Yohann Lepage <yohannlepage@gmail.com>
2009-02-13 10:58:52 +0100

    Ajout de logiciels

--- config/chroot_local-hooks/08-stop.sh
+++ config/chroot_local-hooks/08-stop.sh
@@ -15,3 +15,15 @@ update-rc.d  -f puppetmaster remove
 update-rc.d  puppetmaster stop  0 1 2 3 4 5 6 .
 update-rc.d  -f ssh remove
 update-rc.d  ssh stop  0 1 2 3 4 5 6 .
+update-rc.d  -f pimd remove
+update-rc.d  pimd stop  0 1 2 3 4 5 6 .
+update-rc.d  -f samba remove
+update-rc.d  samba stop  0 1 2 3 4 5 6 .
+update-rc.d  -f freeradius remove
+update-rc.d  freeradius stop  0 1 2 3 4 5 6 .
+update-rc.d  -f squid remove
+update-rc.d  squid stop  0 1 2 3 4 5 6 .
+update-rc.d  -f snmpd remove
+update-rc.d  snmpd stop  0 1 2 3 4 5 6 .
+update-rc.d  -f quagga remove
+update-rc.d  quagga stop  0 1 2 3 4 5 6 .
