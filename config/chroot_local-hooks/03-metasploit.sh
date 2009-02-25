a9ea8a400a1c349da60b84a74086e51451a5d02f
Yohann Lepage <yohannlepage@gmail.com>
2009-02-26 00:59:06 +0100

    modification doc, réorganisation outils

--- config/chroot_local-hooks/03-metasploit.sh
+++ config/chroot_local-hooks/03-metasploit.sh
@@ -6,7 +6,7 @@ echo "###################"
 echo "I: install metasploit"
 
 #mkdir /usr/share/framework-3.1
-cd /usr/share/framework-3.1
+cd /usr/share/framework-3.2
 
 # Téléchargement du paquet
 #wget  http://sugar.metasploit.com/releases/framework-3.1.tar.gz
@@ -17,12 +17,12 @@ cd /usr/share/framework-3.1
 # Création des liens symboliques
 chmod +x msf*
 cd /usr/local/bin
-ln -s /usr/share/framework-3.1/msfcli
-ln -s /usr/share/framework-3.1/msfconsole
-ln -s /usr/share/framework-3.1/msfd
-ln -s /usr/share/framework-3.1/msfencode
-ln -s /usr/share/framework-3.1/msfgui
-ln -s /usr/share/framework-3.1/msfopcode
-ln -s /usr/share/framework-3.1/msfpayload
-ln -s /usr/share/framework-3.1/msfpescan
-ln -s /usr/share/framework-3.1/msfweb
+ln -s /usr/share/framework-3.2/msfcli
+ln -s /usr/share/framework-3.2/msfconsole
+ln -s /usr/share/framework-3.2/msfd
+ln -s /usr/share/framework-3.2/msfencode
+ln -s /usr/share/framework-3.2/msfgui
+ln -s /usr/share/framework-3.2/msfopcode
+ln -s /usr/share/framework-3.2/msfpayload
+ln -s /usr/share/framework-3.2/msfpescan
+ln -s /usr/share/framework-3.2/msfweb
