37a4f0345b8b58574a1a89bfea907f485511cfcf
Yohann Lepage <yohannlepage@gmail.com>
2009-03-12 10:56:06 +0100

    Ajout homepage iceweasel

---
+++ config/chroot_local-hooks/05-iceweasel.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "###################"
+echo "####### 05 ########"
+echo "###################"
+
+echo "pref("browser.startup.homepage", "file:///usr/share/doc/satan-manual/html/index.html");" >> /etc/iceweasel/pref/iceweasel.js
